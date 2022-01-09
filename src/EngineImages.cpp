7da8f7fcac74f04f503c8fd0b29315dc8d3b3b7c
ftk <ftk@users.noreply.github.com>
2022-01-10 00:23:36 +0300

    EngineImages: bicubic scaling

--- src/EngineImages.cpp
+++ src/EngineImages.cpp
@@ -31,6 +31,7 @@ using Gdiplus::FrameDimensionPage;
 using Gdiplus::FrameDimensionTime;
 using Gdiplus::Graphics;
 using Gdiplus::ImageAttributes;
+using Gdiplus::InterpolationModeHighQualityBicubic;
 using Gdiplus::Matrix;
 using Gdiplus::MatrixOrderAppend;
 using Gdiplus::Ok;
@@ -188,6 +189,7 @@ RenderedBitmap* EngineImages::RenderPage(RenderPageArgs& args) {
 
     Graphics g(hDC);
     g.SetCompositingQuality(CompositingQualityHighQuality);
+    g.SetInterpolationMode(InterpolationModeHighQualityBicubic);
     g.SetSmoothingMode(SmoothingModeAntiAlias);
     g.SetPageUnit(UnitPixel);
 
